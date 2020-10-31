afc4c05a47144de66ff06baecca3bb53ee0cd935
Aman Kumar <53273078+shady48765@users.noreply.github.com>
2020-10-31 23:49:10 +0530

    Update CF_contest 236_problemA.cpp
    headers werent included correctly
    cin cout werent used correctly 
    loops error fixed

--- CF_contest 236_problemA.cpp
+++ CF_contest 236_problemA.cpp
@@ -1,14 +1,14 @@
-#includeiostream
-#includestring
-#includecmath
-#includealgorithm
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<algorithm>
 using namespace std;
 int main(){
 	string s;
 	int n=0;
-	cin  s;
-	for (int i = 0; i  s.length(); i++){
-		for (int j = 0; j  i; j++){
+	cin  >>s;
+	for (int i = 0; i < s.length(); i++){
+		for (int j = 0; j <=i; j++){
 			if (s[i] == s[j]){
 				n++;
 				j = i;
@@ -16,9 +16,9 @@ int main(){
 		}
 	}
 	if ((s.length() - n )% 2 == 0)
-		cout  CHAT WITH HER!;
+		cout<<"CHAT WITH HER!";
 	else
-		cout  IGNORE HIM!;
+		cout<<"IGNORE HIM!";
 	return 0;
 
 }
